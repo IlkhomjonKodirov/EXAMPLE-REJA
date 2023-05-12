05e56bead8de58dc09d223509ee9ad3499bc9931
IlkhomjonKodirov <ilhom.qodirov9393@gmail.com>
2023-05-12 07:21:36 -0700

    feat: mongoDB ulanish logic

--- server.js
+++ server.js
@@ -1,47 +1,23 @@
-console.log('Web serverni boshlash');
-const express = require("express");
-const app = express(); 
 const http = require('http');
-const fs = require('fs');
+const mongodb = require('mongodb');
 
-let user;
-fs.readFile("database/user.json", "utf8", (err, data) => {
-  if(err) {
-    console.log("ERROR:", err);
-  }else {
-    user = JSON.parse(data);
+let db;
+const connectionString = "mongodb+srv://Ilkhom:wNwaPSKQ3QxRRRA1@cluster0.t4c9kaq.mongodb.net/Reja?retryWrites=true&w=majority";
+
+mongodb.connect(connectionString, {
+  useNewUrlParser: true, 
+  useUnifiedTopology: true
+}, (err, client) => {
+  if(err) console.log("ERROR on connection MongoDB");
+  else {
+      console.log("MongoDB connection succed");
+      // console.log(client);
+      module.exports = client;
+      const app = require("./app");
+      const server = http.createServer(app);
+      let PORT = 3000;
+      server.listen(PORT, function() {
+        console.log(`The server is running succesfully on port: ${PORT}, http://localhost:${PORT}`);
+    });
   }
 })
-
-// 1 -bosqich: Kirish kodlari 
-app.use(express.static("public")); 
-app.use(express.json());
-app.use(express.urlencoded({extended: true})); 
-
-// 2-bosqich: Sessionga bog'liq kodlar
-
-// 3 - bosqich: viewsga bog'liq kodlar. Beckendda (view) frontend yasaymiz ejs yordamida
-
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// 4 - bosqich: Routingga bog'liq kodlar
-// post - ma'lumotni o'zi bilan olib kelib databasega o'sha ma'lumotni yozadi
-app.post("/create-item", (req, res) => {
-  // console.log(req.body);
-  // console.log(req);
-  // res.json({test: "success"}); // json shaklida ma'lumotni qaytarish(test: success qaytib keladi)
-})
-
-app.get("/author", (req, res) => {
-  res.render("author", {user: user});
-})
-app.get("/", function(req, res) {
-  res.render("reja");
-})
-
-const server = http.createServer(app);
-let PORT = 3000;
-server.listen(PORT, function() {
-  console.log(`The server is running succesfully on port: ${PORT}, http://localhost:${PORT}`);
-});
