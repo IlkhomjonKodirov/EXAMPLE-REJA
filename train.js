957b11b56a617c144877eae70efe22f5b5d97cc5
IlkhomjonKodirov <ilhom.qodirov9393@gmail.com>
2023-05-25 03:23:54 -0700

    feat: Task-E done

--- train.js
+++ train.js
@@ -1,47 +1,86 @@
- 
+/*Task-E: shunday function tuzing unga faqat bitta musbat integer argument qilib berilsin, va u ushbu argumentni tub son bolsa true aksincha false return qilsin.
+
+Masalan: tubSonmi(5) return true, tubSonmi(10) return false. Tub sonlar faqat ozi va 1soniga toliq bolinadigan sonlar. */
+
+function tubSonmi(num) {
+  if(num === 1){
+    return false;
+  }
+  else if(num === 2) {
+    return true;
+  }
+  else {
+    for(let i = 2; i < num; i++) {
+      if(num % i === 0) {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+console.log(tubSonmi(12));
+console.log(tubSonmi(17));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /* D-Task: Shunday class tuzing tuzing nomi Shop, va uni constructoriga 3 hil mahsulot pass bolsin, hamda classning 3ta methodi bolsin, 
 biri qoldiq, biri sotish va biri qabul. Har bir method ishga tushgan vaqt ham log qilinsin.
 
   masalan: const shop = new Shop(4, 5, 2); shop.qoldiq() return hozir 20:40da 4ta non, 5ta lagmon va 2ta cola mavjud! 
 shop.sotish('non', 3) & shop.qabul('cola', 4) & shop.qoldiq() return hozir 20:50da 1ta non, 5ta lagmon va 6ta cola mavjud! */
 
-const moment = require('moment');
-const time = moment().format('HH:mm')
+// const moment = require('moment');
+// const time = moment().format('HH:mm')
 
-class Shop {
-  constructor(non, lagmon, cola) {
-    this.non = non;
-    this.lagmon = lagmon;
-    this.cola = cola;
-  }
+// class Shop {
+//   constructor(non, lagmon, cola) {
+//     this.non = non;
+//     this.lagmon = lagmon;
+//     this.cola = cola;
+//   }
 
-  sotish(productName, num) {
-    if(productName == 'non')
-      this.non -= num;
-    else if( productName == 'lagmon')
-      this.lagmon -= num;
-    else this.cola -= num;
-  }
+//   sotish(productName, num) {
+//     if(productName == 'non')
+//       this.non -= num;
+//     else if( productName == 'lagmon')
+//       this.lagmon -= num;
+//     else this.cola -= num;
+//   }
 
-  qabul(productName, num) {
-    if(productName == 'non')
-      this.non += num;
-    else if(productName == 'lagmon')
-      this.lagmon += num;
-    else this.cola += num;
-  }
+//   qabul(productName, num) {
+//     if(productName == 'non')
+//       this.non += num;
+//     else if(productName == 'lagmon')
+//       this.lagmon += num;
+//     else this.cola += num;
+//   }
 
-  qoldiq(){
-    console.log(`Hozir ${time}da ${this.non}ta non, ${this.lagmon}ta lagmon, ${this.cola}ta cola mavjud!`);
-  }
-}
+//   qoldiq(){
+//     console.log(`Hozir ${time}da ${this.non}ta non, ${this.lagmon}ta lagmon, ${this.cola}ta cola mavjud!`);
+//   }
+// }
 
-const shop = new Shop(4, 5, 8);
-shop.qoldiq();
-shop.sotish('non', 2);
-shop.qabul('lagmon', 1);
-shop.qabul('cola', 1);
-shop.qoldiq();
+// const shop = new Shop(4, 5, 8);
+// shop.qoldiq();
+// shop.sotish('non', 2);
+// shop.qabul('lagmon', 1);
+// shop.qabul('cola', 1);
+// shop.qoldiq();
 
 
 
